aed63e55769b12895e8c3b9f2d25df2e6db22e8f
Ing140943 <skladeeing@gmail.com>
2021-04-01 01:06:00 +0700

    Upload csv file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+ 
   def index
+    @search = params[:search]
     @products = Product.all
+    @products = @products
+      .search(@search) if @search.present?
   end
 end
