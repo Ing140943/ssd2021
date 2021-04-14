09bf4f1f94f526e47226c9e064d5474a8a9a7b48
Ing140943 <skladeeing@gmail.com>
2021-04-15 00:46:52 +0700

    upload image, delete image, using namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+    def article_params
+        params.require(:article).permit(:title, :description, :stock, :status, :category_ids=>[])
+    end
 end
